984d06e6a1387c49fd0a0454c9c3ca6efb53e67c
Raphael Saunier <raphael.saunier@gmail.com>
2026-04-29 20:04:21 +0200

    Fix Burning Mountain redirects and header alignment

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,7 @@
 const BURNING_MOUNTAIN_HOST = "burning-mountain.psy.cards";
 const BURNING_MOUNTAIN_SHORT_HOST = "bm.psy.cards";
+const BURNING_MOUNTAIN_ROUTE = "/burning-mountain";
+const BURNING_MOUNTAIN_INDEX = `${BURNING_MOUNTAIN_ROUTE}/index.html`;
 
 function withPath(request, pathname) {
 	const url = new URL(request.url);
@@ -12,16 +14,20 @@ export default {
 		const url = new URL(request.url);
 
 		if (url.hostname === BURNING_MOUNTAIN_SHORT_HOST) {
+			url.protocol = "https:";
 			url.hostname = BURNING_MOUNTAIN_HOST;
 			return Response.redirect(url, 308);
 		}
 
 		if (url.hostname === BURNING_MOUNTAIN_HOST) {
 			if (url.pathname === "/" || url.pathname === "/index.html") {
-				return env.ASSETS.fetch(withPath(request, "/burning-mountain"));
+				return env.ASSETS.fetch(withPath(request, BURNING_MOUNTAIN_INDEX));
 			}
 
-			if (url.pathname === "/burning-mountain") {
+			if (
+				url.pathname === BURNING_MOUNTAIN_ROUTE ||
+				url.pathname === `${BURNING_MOUNTAIN_ROUTE}/`
+			) {
 				url.pathname = "/";
 				return Response.redirect(url, 308);
 			}
